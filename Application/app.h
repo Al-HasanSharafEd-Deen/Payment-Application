b6cb76e5cd03e81ff287e01e8a36497eaac4325b
Al-Hasan Sharaf Ed-Deen <al-hasanameer.stu.1.14@azhar.edu.eg>
2022-09-25 20:27:20 +0200

    V: 1.0

---
+++ Application/app.h
@@ -0,0 +1,16 @@
+#pragma once 
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+#include "../Card/card.h"
+#include "../Terminal/terminal.h"
+#include "../Server/server.h"
+
+
+/*refere to the flow chart*/
+void appStart(void);
+
+
+#endif // !APPLICATION_H_
+
+
